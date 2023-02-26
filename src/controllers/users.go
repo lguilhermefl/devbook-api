b7e5a7edc688c23c45bb1b72c4145cf7eb02925e
luiz <lguilhermefl@gmail.com>
2023-02-26 17:57:25 -0300

    add comments

--- src/controllers/users.go
+++ src/controllers/users.go
@@ -7,17 +7,17 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Create user!"))
 }
 
-
-func GetUsers(w http.ResponseWriter, r *http.Request) {
+// GetAllUsers get all users
+func GetAllUsers(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Get all users!"))
 }
 
-
+// GetUserById get user by id
 func GetUserById(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Get user by id!"))
 }
 
-
+// UpdateUser update user info
 func UpdateUser(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Update user!"))
 }
