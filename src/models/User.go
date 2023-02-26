27150ee28eb6547ee64e0bd796a0ab178496d17a
luiz <lguilhermefl@gmail.com>
2023-02-26 18:24:10 -0300

    create user model

---
+++ src/models/User.go
@@ -0,0 +1,13 @@
+package models
+
+import "time"
+
+// User represents a user using the app
+type User struct {
+	ID uint64 `json:"id,omitempty"`
+	Name uint64 `json:"name,omitempty"`
+	Nick string  `json:"nick,omitempty"`
+	Email string `json:"email,omitempty"`
+	Password string `json:"password,omitempty"`
+	CreatedAt time.Time `json:"createdAt,omitempty"`
+}
