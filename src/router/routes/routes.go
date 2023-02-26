5fb48cdf541b16450873a10d5426a29da5cb1f24
luiz <lguilhermefl@gmail.com>
2023-02-26 18:03:16 -0300

    config routes

--- src/router/routes/routes.go
+++ src/router/routes/routes.go
@@ -1,6 +1,10 @@
 package routes
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 // Route represents all api routes
 type Route struct {
@@ -8,4 +12,15 @@ type Route struct {
 	Method string
 	Function func(http.ResponseWriter, *http.Request)
 	RequireAuth bool
+}
+
+// Config puts all routes in router
+func Config(r *mux.Router) *mux.Router {
+	routes := userRoutes
+
+	for _, route := range routes {
+		r.HandleFunc(route.URI, route.Function).Methods(route.Method)
+	}
+
+	return r;
 }
