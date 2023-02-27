3007592cd966648ddef93d0b218538e307b3e04c
luiz <lguilhermefl@gmail.com>
2023-02-27 10:07:04 -0300

    hash password on sign up

--- go.mod
+++ go.mod
@@ -9,4 +9,7 @@ require (
 	github.com/joho/godotenv v1.5.1
 )
 
-require github.com/badoux/checkmail v1.2.1 // indirect
+require (
+	github.com/badoux/checkmail v1.2.1
+	golang.org/x/crypto v0.6.0
+)
--- src/models/User.go
+++ src/models/User.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"api/src/security"
 	"errors"
 	"strings"
 	"time"
@@ -23,7 +24,9 @@ func (user *User) Prepare(stage string) error {
 		return err
 	}
 
-	user.format()
+	if err := user.format(stage); err != nil {
+		return err
+	}
 	return nil
 }
 
@@ -51,8 +54,19 @@ func (user *User) validate(stage string) error {
 	return nil
 }
 
-func (user *User) format() {
+func (user *User) format(stage string) error {
 	user.Name = strings.TrimSpace(user.Name)
 	user.Nick = strings.TrimSpace(user.Nick)
 	user.Email = strings.TrimSpace(user.Email)
+
+	if stage == "create" {
+		passwordHash, err := security.Hash(user.Password)
+		if err != nil {
+			return err
+		}
+
+		user.Password = string(passwordHash)
+	}
+
+	return nil
 }
