37cef89bcf1189104ce0f60250fe95f0235f8224
luiz <lguilhermefl@gmail.com>
2023-02-28 08:57:50 -0300

    add post validation body on creation

--- src/models/Post.go
+++ src/models/Post.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 type Post struct {
 	ID uint64 `json:"id,omitempty"`
@@ -10,4 +14,30 @@ type Post struct {
 	AuthorNick string `json:"authorNick,omitempty"`
 	Likes uint64 `json:"likes"`
 	CreatedAt time.Time `json:"createdAt,omitempty"`
+}
+
+func (post *Post) Prepare() error {
+	if err := post.validate(); err != nil {
+		return err
+	}
+
+	post.format()
+	return nil
+}
+
+func (post *Post) validate() error {
+	if post.Title == "" {
+		return errors.New("Title is required")
+	}
+
+	if post.Content == "" {
+		return errors.New("Content is required")
+	}
+
+	return nil
+}
+
+func (post *Post) format() {
+	post.Title = strings.TrimSpace(post.Title)
+	post.Content = strings.TrimSpace(post.Content)
 }
