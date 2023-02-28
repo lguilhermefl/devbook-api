f77c0b2240ecc8acb0ba4233c2edd72c1e672dcc
luiz <lguilhermefl@gmail.com>
2023-02-28 08:07:48 -0300

    add posts routes

---
+++ src/router/routes/posts.go
@@ -0,0 +1,39 @@
+package routes
+
+import (
+	"api/src/controllers"
+	"net/http"
+)
+
+var postRoutes = []Route{
+	{
+		URI: "/posts",
+		Method: http.MethodPost,
+		Function: controllers.CreatePost,
+		RequireAuth: true,
+	},
+	{
+		URI: "/posts",
+		Method: http.MethodGet,
+		Function: controllers.GetPosts,
+		RequireAuth: true,
+	},
+	{
+		URI: "/posts/{postId}",
+		Method: http.MethodGet,
+		Function: controllers.GetPostById,
+		RequireAuth: true,
+	},
+	{
+		URI: "/posts/{postId}",
+		Method: http.MethodPut,
+		Function: controllers.UpdatePost,
+		RequireAuth: true,
+	},
+	{
+		URI: "/posts/{postId}",
+		Method: http.MethodDelete,
+		Function: controllers.DeletePost,
+		RequireAuth: true,
+	},
+}
